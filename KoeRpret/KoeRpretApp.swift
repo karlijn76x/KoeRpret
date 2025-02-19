97c447f9593ab866ccf3e1f8d9fa3d62787928c9
karlijn76x <karlijnvandenherik@gmail.com>
2025-02-19 12:20:32 +0100

    Initial Commit

---
+++ KoeRpret/KoeRpretApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoeRpretApp.swift
+//  KoeRpret
+//
+//  Created by Karlijn van den Herik on 19/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct KoeRpretApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
