69ec603ad11d596d71a770950e0d2ff30a660224
Jasmin Hachmane <509304@student.fontys.nl>
2025-02-25 11:42:30 +0100

    I added the background on every page. I added the icons for the cow. With components

---
+++ KoeRpret/Views/BackgroundSquare.swift
@@ -0,0 +1,18 @@
+//
+//  BackgroundSquare.swift
+//  KoeRpret
+//
+//  Created by Jasmin Hachmane on 25/02/2025.
+//
+
+import SwiftUI
+
+struct BackgroundSquare: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackgroundSquare()
+}
