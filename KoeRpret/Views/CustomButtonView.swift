3ec5776f876c5b3313a17c21d1e7529e75a60e27
karlijn76x <karlijnvandenherik@gmail.com>
2025-02-25 11:47:00 +0100

    Added button and finished startscreen

---
+++ KoeRpret/Views/CustomButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomButtonView.swift
+//  KoeRpret
+//
+//  Created by Karlijn van den Herik on 25/02/2025.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomButtonView()
+}
