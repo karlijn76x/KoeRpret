d57c557f6aa61dafe0ffbf77119ac1fe4c8f8100
Jasmin Hachmane <509304@student.fontys.nl>
2025-02-25 14:37:53 +0100

    Home + Welcome page

--- KoeRpret/Views/HomeView.swift
+++ KoeRpret/Views/HomeView.swift
@@ -1,17 +1,59 @@
-//
-//  HomeView.swift
-//  KoeRpret
-//
-//  Created by Karlijn van den Herik on 21/02/2025.
-//
-
 import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ZStack {
-            BackGroundView()
-            BackgroundSquare()
+        GeometryReader { geometry in
+            ZStack {
+                BackGroundView()
+                BackgroundSquare()
+                
+                
+                Text("Boe-gin de pret 🔐")
+                    .font(.custom("PatrickHandSC-Regular", size: 50))
+                    .foregroundColor(.black)
+                    .padding()
+                    .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.04)
+
+                VStack(spacing: 60) {  // The space between the buttons
+                    
+                    // First button
+                    Button(action: {
+                        print("Button 1 clicked")
+                    }) {
+                        Rectangle()
+                            .fill(Color(red: 189/255, green: 217/255, blue: 242/255)) // Background color
+                            .frame(width: 250, height: 270)  // Increased size for the button
+                            .cornerRadius(15)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(Color(red: 174/255, green: 194/255, blue: 159/255), lineWidth: 3) // Border color
+                            )
+                            .overlay(
+                                Text("📷")
+                                    .font(.system(size: 130))
+                            )
+                    }
+                    
+                    // Second button
+                    Button(action: {
+                        print("Button 2 clicked")
+                    }) {
+                        Rectangle()
+                            .fill(Color(red: 189/255, green: 217/255, blue: 242/255))
+                            .frame(width: 250, height: 270)
+                            .cornerRadius(15)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 15)
+                                    .stroke(Color(red: 174/255, green: 194/255, blue: 159/255), lineWidth: 3)
+                            )
+                            .overlay(
+                                Text("🏆")
+                                    .font(.system(size: 130))
+                            )
+                    }
+                }
+                .position(x: geometry.size.width * 0.5, y: geometry.size.height * 0.55) 
+            }
         }
     }
 }
@@ -19,3 +61,4 @@ struct HomeView: View {
 #Preview {
     HomeView()
 }
+
